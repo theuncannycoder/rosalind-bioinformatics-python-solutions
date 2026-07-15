4603457dbeb9b34a0c6d0c1e826d018a86c7b2f8
that coder girlie <srishtii.148@gmail.com>
2026-07-15 21:38:15 +0530

    Create dna.py

---
+++ dna.py
@@ -0,0 +1,11 @@
+#Problem: Counting DNA nucleotides
+#Rosalind ID: DNA
+
+s = input().strip()
+count_A = s.count('A')
+count_C = s.count('C')
+count_G = s.count('G')
+count_T = s.count('T')
+
+
+print(count_A, count_C, count_G, count_T)
